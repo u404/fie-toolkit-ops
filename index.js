ddd2bbdd46707205e9854ac349449c400b0b7e3f
zhangguanghui <zhangguanghui@smartcinema.com.cn>
2020-09-23 15:31:03 +0800

    "init"

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict"
+
+module.exports = {
+
+  init: require("./lib/init"),
+
+  start: require("./lib/start"),
+
+  build: require("./lib/build"),
+
+  debug: require("./lib/debug"),
+
+  help: require("./lib/help")
+
+}
